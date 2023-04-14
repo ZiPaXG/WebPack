ee5ef000533f74e827a95eb948fd772f81665d24
ZiPaXG <shepelev.danilka@bk.ru>
2023-04-14 18:50:22 +0300

    Edit day.jsx

--- app/components/day.jsx
+++ app/components/day.jsx
@@ -27,7 +27,7 @@ class Day extends React.Component
                             ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"].map(
                                 function(time, index)
                                 {
-                                    return <LessonRow time={time} subject={" "}/>
+                                    return <LessonRow time={time} subject={" "} key={"lesson-row-"+index}/>
                                 }
                             )
                         }
